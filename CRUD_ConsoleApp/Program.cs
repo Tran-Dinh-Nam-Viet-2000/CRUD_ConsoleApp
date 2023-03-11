ec13ed5bbf4e77c1b5d0396c3b598a013336a2b5
viettdnde140214 <viettdnde140214@outlook.com>
2023-03-11 23:20:18 +0700

    fisrt commit

---
+++ CRUD_ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUD_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
